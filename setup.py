8406e761b5578e7043049290b9482faeb084db6b
James Dabbs <james.dabbs@gmail.com>
2012-05-27 13:34:12 -0400

    Limited table summary to those spaces that are in Counterexamples.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brubeck',
-    version='0.0.1.02',
+    version='0.0.1.03',
     description='Django-powered database of topological information',
 
     author='James Dabbs',
