10e08c71d375e4db4b284e3e330f36a9b14e71a3
James Dabbs <james.dabbs@gmail.com>
2012-05-27 20:26:12 -0400

    Bumped version number for push

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brubeck',
-    version='0.0.1.07',
+    version='0.0.1.08',
     description='Django-powered database of topological information',
 
     author='James Dabbs',
