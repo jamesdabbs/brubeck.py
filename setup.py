747d32790c06708ad7d2a49326548c0b9f42d8b9
James Dabbs <james.dabbs@gmail.com>
2012-05-29 07:01:01 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brubeck',
-    version='0.0.1.017',
+    version='0.0.1.018',
     description='Django-powered database of topological information',
 
     author='James Dabbs',
