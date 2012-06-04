181f03e08c59e37c4b992d47bf2ee02c69f40dd4
James Dabbs <james.dabbs@gmail.com>
2012-06-04 16:27:10 -0400

    Bumped version number for push to Heroku

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brubeck',
-    version='0.0.1.030',
+    version='0.0.1.031',
     description='Django-powered database of topological information',
 
     author='James Dabbs',
