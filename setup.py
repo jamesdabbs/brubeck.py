b03d8e903e43eb2b1a5be7743eb336043f128ca8
James Dabbs <james.dabbs@gmail.com>
2012-06-16 22:18:17 -0400

    Fixed search template

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brubeck',
-    version='0.0.1.038',
+    version='0.0.1.039',
     description='Django-powered database of topological information',
 
     author='James Dabbs',
